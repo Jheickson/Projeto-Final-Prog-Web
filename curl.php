026b62ec7590fbcc576047d1a80885b9b6beab7c
Jheickson <felipesantosatm@outlook.com>
2022-06-16 06:59:44 -0300

    Update curl.php

--- curl.php
+++ curl.php
@@ -2,7 +2,7 @@
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, "https://api.isthereanydeal.com/v01/deals/list/?key=c22b79a6422708915183a0b4ae493ffc128a8e25&limit=15&offset=0&region=br2&country=BR&shops=steam%2Cgog&sort=price%3Aasc");
+curl_setopt($ch, CURLOPT_URL, "https://api.isthereanydeal.com/v01/deals/list/?key=c22b79a6422708915183a0b4ae493ffc128a8e25&limit=15&offset=0&region=br2&country=BR&shops=steam%2Cgog&sort=time%3Adesc");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 curl_setopt($ch, CURLOPT_HEADER, FALSE);
 
