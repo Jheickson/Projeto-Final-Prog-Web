993981770dab13d0136b6d141a3f2e88d26d4a0c
Jheickson <felipesantosatm@outlook.com>
2022-06-23 19:59:29 -0300

    limpeza de projeto
    Deletando algumas coisas que não seriam utilizadas no projeto

--- curl.php
+++ curl.php
@@ -1,19 +1,28 @@
 <?php
 
-$ch = curl_init();
+//include_once("index.php");
 
-curl_setopt($ch, CURLOPT_URL, "https://api.isthereanydeal.com/v01/deals/list/?key=c22b79a6422708915183a0b4ae493ffc128a8e25&limit=30&offset=0&region=br2&country=BR&shops=steam&sort=time%3Adesc");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($ch, CURLOPT_HEADER, FALSE);
+// $url = "https://api.isthereanydeal.com/v01/deals/list/?key=c22b79a6422708915183a0b4ae493ffc128a8e25&limit=15&offset=0&region=br2&country=BR&shops=steam&sort=time%3Adesc";
 
-$response = curl_exec($ch);
-curl_close($ch);
 
-//var_dump($response);
-$response_arr = json_decode($response);
+// $ch = curl_init();
 
-//echo $response_arr;
+// //URL default
+// // curl_setopt($ch, CURLOPT_URL, "https://api.isthereanydeal.com/v01/deals/list/?key=c22b79a6422708915183a0b4ae493ffc128a8e25&limit=30&offset=0&region=br2&country=BR&shops=steam&sort=time%3Adesc");
+// curl_setopt($ch, CURLOPT_URL, $url);
+// curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+// curl_setopt($ch, CURLOPT_HEADER, FALSE);
 
-//var_dump($response_arr);
+// $response = curl_exec($ch);
+// curl_close($ch);
+
+
+
+// //var_dump($response);
+// $response_arr = json_decode($response);
+
+// //echo $response_arr;
+
+// //var_dump($response_arr);
 
 ?>
--- index.php
+++ index.php
@@ -11,8 +11,6 @@
 		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 		<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-indigo.css">
 		<link rel="stylesheet" href="style.css">
-		<script src="js/scripts.js">
-		</script>
 
 	</head>
 
@@ -31,14 +29,14 @@
 			</div>
 
 		<a href="https://github.com/Jheickson/Projeto-Final-Prog-Web" target="blank" class="w3-bar-item w3-button" style="width:100%;">Link para o projeto no Github<br>
-	
+
 		<img src="https://cdn-icons-png.flaticon.com/512/1051/1051326.png" width="50%" height="50%" alt="Github" title="Github">
 
 	</a>
 		<a href="https://github.com/Jheickson" target="blank" class="w3-bar-item w3-button" style="width:100%;">Perfil do discente no Github <br>
-	
+
 		<img id="fotoPerfil" src="img/Felipe.jpg" width="50%" height="50%" alt="Jheickson Felipe" title="Jheickson Felipe">
-	
+
 
 	</a>
 		<a href="sobre.php" class="w3-bar-item w3-button" style="width:100%;">Sobre</a>
@@ -56,62 +54,73 @@
 
 				<?php
 
-					include_once("curl.php");
+					function updateCurl($offset){
 
-					foreach ($response_arr->data->list as $item){
+						$url = "https://api.isthereanydeal.com/v01/deals/list/?key=c22b79a6422708915183a0b4ae493ffc128a8e25&limit=75&offset=$offset&region=br2&country=BR&shops=steam&sort=time%3Adesc";
 
-						$titulo = $item->title;
-						$preco_novo = "R$ " . number_format($item->price_new, 2);
-						$preco_velho = "R$ " . number_format($item->price_old, 2);
-						$preco_desconto = $item->price_cut;
-						$preco_descontado = "R$ " . number_format(($item->price_old) * ($item->price_cut / 100), 2);
-						$link = $item->urls->buy;
-						$steamID = (int)filter_var($link, FILTER_SANITIZE_NUMBER_INT);
 
-						echo '
+						$ch = curl_init();
 
-							<div class="w3-third w3-card w3-margin-bottom w3-row-padding">
+						curl_setopt($ch, CURLOPT_URL, $url);
+						curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+						curl_setopt($ch, CURLOPT_HEADER, FALSE);
 
-							<div class="parent">
-								<a href="' , $link , '" target="blank"> <img src="https://via.placeholder.com/616x353?text=Imagem+Não+Encontrada" alt="',$titulo,'" style="width: 100%;" class="w3-hover-capacity w3-container"> </a>
+						$response = curl_exec($ch);
+						curl_close($ch);
 
-								<a href="' , $link , '" target="blank"> <img id="gameHeader" src="https://cdn.cloudflare.steamstatic.com/steam/apps/', $steamID ,'/capsule_616x353.jpg" alt="',$titulo,'" style="width: 100%;" class="over-img w3-hover-capacity w3-container" onerror=" <script>teste(); <\script>"> </a>
-							</div>
 
-							<div class="w3-container w3-white" style="width: 100%">
+						//var_dump($response);
+						$response_arr = json_decode($response);
+						//var_dump($response_arr);
 
-								<p style="font-size: 1em"><b> ' , $titulo ,'</b><br>
-								Preço Atual: ' , $preco_novo ,' <br>
-								' , $preco_desconto , '% de Desconto (' , $preco_descontado , ')<br>
-								Preço Normal: ' , $preco_velho , '</p>
 
-							</div>
-							
-							</div>
-							
-						';
+						foreach ($response_arr->data->list as $item){
+
+							$titulo = $item->title;
+
+							if (strlen($titulo) > 30){
+
+								$titulo = substr($titulo , 0 , 30) . "...";
+
+							}
+
+							$preco_novo = "R$ " . number_format($item->price_new, 2);
+							$preco_velho = "R$ " . number_format($item->price_old, 2);
+							$preco_desconto = $item->price_cut;
+							$preco_descontado = "R$ " . number_format(($item->price_old) * ($item->price_cut / 100), 2);
+							$link = $item->urls->buy;
+							$steamID = (int)filter_var($link, FILTER_SANITIZE_NUMBER_INT);
+
+							echo '
+
+								<div class="w3-third w3-card w3-margin-bottom w3-row-padding">
+
+								<div class="parent">
+									<a href="' , $link , '" target="blank"> <img src="https://via.placeholder.com/616x353?text=Imagem+Não+Encontrada" alt="',$titulo,'" style="width: 100%;" class="w3-hover-capacity w3-container"> </a>
+
+									<a href="' , $link , '" target="blank"> <img id="gameHeader" src="https://cdn.cloudflare.steamstatic.com/steam/apps/', $steamID ,'/capsule_616x353.jpg" alt="',$titulo,'" style="width: 100%;" class="over-img w3-hover-capacity w3-container" onerror=" <script>teste(); <\script>"> </a>
+								</div>
+
+								<div class="w3-container w3-white" style="width: 100%">
+
+									<p style="font-size: 1em"><b> ' , $titulo ,'</b><br>
+									Preço Atual: ' , $preco_novo ,' <br>
+									' , $preco_desconto , '% de Desconto (' , $preco_descontado , ')<br>
+									Preço Normal: ' , $preco_velho , '</p>
+
+								</div>
+								
+								</div>
+								
+							';
+
+						}
 
 					}
 
+					updateCurl(0);
+
 				?>
-
-
-				<!-- Paginação -->
-
-				<div class="w3-center">
-
-					<div class="w3-bar">
-
-						<a href="#" class="w3-bar-item w3-button">&laquo;</a>
-						<a href="#" class="w3-button">1</a>
-						<a href="#" class="w3-button">2</a>
-						<a href="#" class="w3-button">3</a>
-						<a href="#" class="w3-button">4</a>
-						<a href="#" class="w3-button">&raquo;</a>
-
-					</div>
-
-				</div>
 				
 				<!-- Rodapé -->
 				<div class="w3-row w3-container">
@@ -125,8 +134,6 @@
 					</div>
 
 				</div>
-
-
 
 			</div>
 
