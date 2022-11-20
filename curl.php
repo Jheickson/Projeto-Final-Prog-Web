9d486bfe4fb7b66d8b60efb1a358fba57fa3daf4
Jheickson Felipe <102704500+Jheickson@users.noreply.github.com>
2022-11-20 02:26:09 -0300

    Remove key

--- curl.php
+++ curl.php
@@ -2,13 +2,13 @@
 
 //include_once("index.php");
 
-// $url = "https://api.isthereanydeal.com/v01/deals/list/?key=c22b79a6422708915183a0b4ae493ffc128a8e25&limit=15&offset=0&region=br2&country=BR&shops=steam&sort=time%3Adesc";
+// $url = "https://api.isthereanydeal.com/v01/deals/list/?key=(Removed Key)&limit=15&offset=0&region=br2&country=BR&shops=steam&sort=time%3Adesc";
 
 
 // $ch = curl_init();
 
 // //URL default
-// // curl_setopt($ch, CURLOPT_URL, "https://api.isthereanydeal.com/v01/deals/list/?key=c22b79a6422708915183a0b4ae493ffc128a8e25&limit=30&offset=0&region=br2&country=BR&shops=steam&sort=time%3Adesc");
+// // curl_setopt($ch, CURLOPT_URL, "https://api.isthereanydeal.com/v01/deals/list/?key=(Removed Key)e25&limit=30&offset=0&region=br2&country=BR&shops=steam&sort=time%3Adesc");
 // curl_setopt($ch, CURLOPT_URL, $url);
 // curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 // curl_setopt($ch, CURLOPT_HEADER, FALSE);
